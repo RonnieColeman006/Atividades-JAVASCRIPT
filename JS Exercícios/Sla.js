e42105f8443d0e4a0eb1bdebe959416ea60c6cf0
Gabriel <162443958+RonnieColeman006@users.noreply.github.com>
2024-06-12 08:26:32 -0300

    Add files via upload

---
+++ JS Exercícios/Sla.js
@@ -0,0 +1,44 @@
+function boasVindas(){
+    console.log("Boas Vindas!")
+}
+boasVindas()
+
+function somarNumeros(){
+    let a=6,b=2,soma
+    soma=a+b
+    console.log(soma)
+
+}
+somarNumeros()
+
+function subtrairNumeros(){
+    let a=6,b=2,subtração
+    subtração=a-b
+    console.log(subtração)
+
+}
+subtrairNumeros()
+
+function multiplicarNumeros(){
+    let a=6,b=2,multiplicação
+    multiplicação=a*b
+    console.log(multiplicação)
+
+}
+multiplicarNumeros()
+
+function dividirNumeros(){
+    let a=6,b=2,divisão
+    divisão=a/b
+    console.log(divisão)
+
+}
+dividirNumeros()
+
+function calcularMedia(){
+    let a=5,b=2,c=7,media
+    media=(a+b+c)/3
+    console.log(media)
+
+}
+calcularMedia()
